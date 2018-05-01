a6925e76ba4ac777dd9656c2beff1faa9403c76e
Tim Gubner <tim.gubner@cwi.nl>
2018-05-01 11:25:50 +0200

    Factored out JSON config

---
+++ conf.hpp
@@ -0,0 +1,9 @@
+#ifndef H_GEN_CONF
+#define H_GEN_CONF
+
+#include <string>
+#include "spec.hpp"
+
+void parse_config(std::string&& fname, RelSpec& spec);
+
+#endif
--- gen.cpp
+++ gen.cpp
@@ -1,77 +1,10 @@
 #include "spec.hpp"
 
-#include <thread>
 #include <iostream>
-#include <boost/property_tree/ptree.hpp>
-#include <boost/property_tree/json_parser.hpp>
 #include <boost/program_options.hpp>
-#include <boost/algorithm/string.hpp>
 
+#include "conf.hpp"
 #include "generator.hpp"
-
-struct InvalidConf : std::exception {
-	const char* msg;
-	InvalidConf(const char* msg) : msg(msg) {
-	}
-
-	virtual const char* what() const throw() {
-		return msg;
-	}
-};
-
-void parse_config(std::string&& fname, RelSpec& spec) {
-	// Short alias for this namespace
-	namespace pt = boost::property_tree;
-
-	// Create a root
-	pt::ptree root;
-
-	// Load the json file in this ptree
-	pt::read_json(fname, root);
-
-	spec.card = root.get<size_t>("tuples", 0);
-	spec.threads = root.get<size_t>("threads", std::thread::hardware_concurrency());
-
-	for (pt::ptree::value_type &col : root.get_child("columns")) {
-		auto parse_ctype = [] (std::string& v) {
-			if (boost::iequals(v, "integer") || boost::iequals(v, "int")) {
-				return Integer;
-			}
-			if (boost::iequals(v, "string") || boost::iequals(v, "str")) {
-				return String;
-			}
-			throw InvalidConf("invalid gdata.type");
-		};
-
-		auto parse_cgen = [] (std::string& v) {
-			if (boost::iequals(v, "sequential")) {
-				return Sequential;
-			}
-			if (boost::iequals(v, "random")) {
-				return Random;
-			}
-			throw InvalidConf("invalid gen.type");
-		};
-
-		auto parse_col = [&] (pt::ptree& node) -> ColSpec {
-			ColSpec r;
-			std::string str;
-
-			r.min = node.get<int64_t>("min", 0);
-			r.max = node.get<int64_t>("max", 0);
-
-			str = node.get<std::string>("data.type");
-			r.ctype = parse_ctype(str);
-
-			str = node.get<std::string>("gen.type");
-			r.cgen = parse_cgen(str);
-
-			return std::move(r);
-		};
-
-		spec.cols.push_back(parse_col(col.second));
-	}
-}
 
 int main(int argc, char* argv[]) {
 	std::ios_base::sync_with_stdio(false);
