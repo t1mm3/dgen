0f9db16790d5482e6e839bce4b071c288a98738c
Tim Gubner <tim.gubner@cwi.nl>
2018-05-01 22:54:49 +0200

    COnfiguration test

---
+++ test/config_test.cpp
@@ -0,0 +1,14 @@
+#define BOOST_TEST_MODULE "Config"
+
+#include <boost/test/unit_test.hpp>
+
+#include "conf.hpp"
+#include "test/buildpaths.hpp"
+#include <iostream>
+
+BOOST_AUTO_TEST_CASE(json_parse) {
+	RelSpec rel;
+
+	parse_config(std::string(g_path) + std::string("json_conf1.txt"),
+		rel);
+}
