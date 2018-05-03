18d181c19b7229fb3336022ccf552598590d5c15
Tim Gubner <tim.gubner@cwi.nl>
2018-05-03 10:47:16 +0200

    Basic benchmark runner

--- test/bench.py
+++ test/bench.py
@@ -1,2 +1,17 @@
 #!/bin/env python
+from buildpaths import (getProjectBinPath, getTestPath)
+import time
+import os
+import subprocess
 
+def run(config):
+	command = getProjectBinPath() + "/dgen"
+	config = getTestPath() + config
+
+	with open(os.devnull, "w") as f:
+		start_time = time.time()
+		subprocess.call([command, config], stdout=f)
+		print("--- %s seconds ---" % (time.time() - start_time))
+
+
+run("bench_conf1.txt")
