03bc4222596a7964f5a214651965c70f08df41e4
Tim Gubner <tim.gubner@cwi.nl>
2018-05-03 11:23:04 +0200

    Two benchmarks

--- test/bench.py
+++ test/bench.py
@@ -1,17 +1,107 @@
-#!/bin/env python
+#!/bin/env python2
 from buildpaths import (getProjectBinPath, getTestPath)
 import time
 import os
 import subprocess
+import tempfile
 
-def run(config):
+def gen_csv(col_domains, num_tuples, file):
+	cols = ""
+	first = True
+	for c in col_domains:
+		col = ""
+		cgen_str = ""
+
+		(cmin, cmax, cgen, ctype) = c
+		if cgen == ColGenType.RANDOM:
+			cgen_str = "random"
+		elif cgen == ColGenType.SEQUENTIAL:
+			cgen_str = "sequential"
+		else:
+			assert(False)
+
+		if ctype == ColType.INT:
+			col = '{{ "integer" : {{ "gen" : "{gen}", "min" : {min}, "max" : {max} }} }}'.format(gen=cgen_str, min=cmin, max=cmax)
+		elif ctype == ColType.STR:
+			col = '{{ "string" : {{"index" : {{ "integer" : {{ "gen" : "{gen}", "min" : {min}, "max" : {max} }} }}, "file" : "words.txt" }} }}'.format(gen=cgen_str, min=cmin, max=cmax)
+		else:
+			assert(False)
+
+		# concat
+		if first:
+			first = False
+			cols = col
+		else:
+			cols = cols + ", \n" + col
+
+	script = """
+{{
+    "version" : "0.1",
+    "tuples" : {num},
+    "columns" : [
+{cols}
+    ],
+    "format" : {{
+        "csv" : {{ "comma" : "|", "newline" : "|\\n"}}
+    }},
+    "output" : "stdout"
+}}
+
+""".format(num=num_tuples, cols=cols)
+
+	read, write = os.pipe()
+	os.write(write, script)
+	os.close(write)
+	with open(file,"wb") as out:
+		subprocess.Popen("dgen",stdout=out, stdin=read)
+
+
+def run(script, descr):
 	command = getProjectBinPath() + "/dgen"
-	config = getTestPath() + config
 
-	with open(os.devnull, "w") as f:
-		start_time = time.time()
-		subprocess.call([command, config], stdout=f)
-		print("--- %s seconds ---" % (time.time() - start_time))
+	with tempfile.NamedTemporaryFile() as temp:
+		temp.write(script)
+		temp.flush()
 
+		with open(os.devnull, "w") as f:
+			start_time = time.time()
+			subprocess.call(["perf", "record", "--", command, temp.name], stdout=f)
+			print("%s\t%s" % (descr, time.time() - start_time))
 
-run("bench_conf1.txt")
+run("""
+{{
+    "version" : "0.1",
+    "tuples" : 10000000,
+    "threads" : 1,
+    "columns" : [
+        {{ "integer" : {{ "gen" : "random", "min" : 10, "max" : 10000000 }} }}
+    ],
+    "format" : {{
+        "csv" : {{ "comma" : "|", "newline" : "\\n"}}
+    }},
+    "output" : "stdout"
+}}
+	""".format(), "1 int")
+
+int1024 = ""
+first = True
+for i in range(0, 1024):
+	sep = "" if first else ",\n"
+
+	int1024 = int1024 + """{sep}{{ "integer" : {{ "gen" : "random", "min" : 10, "max" : 10000000 }} }}""".format(sep=sep)
+	first = False
+
+run("""
+{{
+    "version" : "0.1",
+    "tuples" : 10000,
+    "threads" : 1,
+    "columns" : [
+        {ints}
+    ],
+    "format" : {{
+        "csv" : {{ "comma" : "|", "newline" : "\\n"}}
+    }},
+    "output" : "stdout"
+}}
+	""".format(ints=int1024), "1024 int")
