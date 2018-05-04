fce66ff4f76c08ef80bea4636fea06e109766e51
Tim Gubner <tim.gubner@cwi.nl>
2018-05-04 15:43:22 +0200

    Added macro test for cardinalites

---
+++ test/cards.py
@@ -0,0 +1,25 @@
+#!/bin/env python2
+from buildpaths import (getProjectBinPath, getTestPath)
+from subprocess import Popen, PIPE
+
+def test_card(num):
+	p = Popen([
+			getProjectBinPath() + "/dgen",
+			getTestPath() + "/json_conf1.txt",
+			"-t", "32",
+			"-n", str(num)
+		], stdout=PIPE)
+	lines = sum(1 for _ in p.stdout)
+	print ("{num} == {lines}".format(num=num, lines=lines))
+	assert(num == lines)
+
+print("test_card")
+
+card = 1
+for c in range(0, 10):
+	test_card(card)
+	card = card * 10
+
+
+
+test_card(0)
