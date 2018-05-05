02ac1971a586d66e842f0d5c57e13ab9518b1ae3
Tim Gubner <tim.gubner@cwi.nl>
2018-05-05 14:53:01 +0200

    Run diff tests single-threaded and parallel

--- test/diff_test.py
+++ test/diff_test.py
@@ -5,11 +5,12 @@ from subprocess import Popen, PIPE
 import sys
 from multiprocessing import Pool
 
-def diff(file):
+def diff(opts):
+	(file, threads) = opts
 	gen = getProjectBinPath() + "/dgen"
 	config = getTestPath() + "/" + file
 
-	data = Popen([gen, config], stdout=PIPE)
+	data = Popen([gen, config, "-t", str(threads)], stdout=PIPE)
 	diff = Popen(['diff', config + ".expect", "-"],stdin=data.stdout, stdout=PIPE)
 	data.stdout.close()
 	out, err = diff.communicate()
@@ -17,10 +18,15 @@ def diff(file):
 	out = out.decode().strip()
 	return(out == "")
 
-if __name__ == '__main__':
-	tests = ["json_conf1.txt", "json_conf2.txt", "json_conf3.txt", "json_conf1large.txt",
-		"json_conf2large.txt", "json_conf3large.txt", "json_conf4large.txt"]
+def run(testnames, parallel):
+	# generate tests
+	tests = []
+	for testname in testnames:
+		for threads in range(1, 16):
+			t = 16 if parallel else 1
+			tests = tests + [(testname, t)]
 
+	# run tests
 	pool = Pool(processes=4)
 	result = pool.map(diff, tests)
 
@@ -32,8 +38,18 @@ if __name__ == '__main__':
 			all_good = False
 			print("Test '{}' failed".format(test))
 
-	if all_good:
-		print("Success")
-		exit(0)
-	else:
+	if not all_good:
 		exit(1)
+
+if __name__ == '__main__':
+	testnames = ["json_conf1.txt", "json_conf2.txt", "json_conf3.txt", "json_conf1large.txt",
+		"json_conf2large.txt", "json_conf3large.txt", "json_conf4large.txt"]
+
+	print("Single threaded")
+	run(testnames, False)
+
+	print("Parallel")
+	run(testnames, True)
+
+	print("Success")
+	exit(0)
