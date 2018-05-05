c12a9c9257ef7556ac24590005c7925351620246
Tim Gubner <tim.gubner@cwi.nl>
2018-05-05 13:42:35 +0200

    Large tests for diff

--- test/diff_test.py
+++ test/diff_test.py
@@ -8,18 +8,23 @@ from subprocess import check_output
 def diff(file):
 	gen = getProjectBinPath() + "/dgen"
 	config = getTestPath() + "/" + file
-	
+
 	data = Popen([gen, config], stdout=PIPE)
 	diff = Popen(['diff', config + ".expect", "-"],stdin=data.stdout, stdout=PIPE)
 	data.stdout.close()
 	out, err = diff.communicate()
 
+	out = out.decode().strip()
 	print(out)
-	assert(out.strip == "")
+	assert(out == "")
 
 diff("json_conf1.txt")
 diff("json_conf2.txt")
 diff("json_conf3.txt")
+diff("json_conf1large.txt")
+diff("json_conf2large.txt")
+diff("json_conf3large.txt")
+diff("json_conf4large.txt")
 
 print("Success")
 exit(0)
